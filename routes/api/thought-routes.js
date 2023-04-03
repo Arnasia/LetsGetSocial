a6b32f4288c7275f443f1f82b31ab890e2ac7aee
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-04-03 15:57:05 -0400

    added Api routes and exporting

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -9,3 +9,33 @@ const { getAllThought,
     removeReaction,
 } = require('../../controllers/Thought-controller');
 
+// api/thoughts
+router
+.route('/')
+.get(getAllThought)
+
+
+// api/thoughts/:id
+router
+.route("/:userId")
+.post(addThought)
+
+// specific thought  /:thoughtId
+router
+.route('/:thoughtId')
+.get(getThoughtById)
+.put(updateThought)
+
+// 
+router
+.route('/userId/:thoughtId')
+.delete(removeThought);
+
+// api/thoughts/:thoughtId/reaction
+router.route("/:thoughtId/reaction")
+.post(addReaction);
+
+// reactionId
+router.route("/:thoughtId/reaction/:reactionId").delete(removeReaction);
+
+module.exports = router;
