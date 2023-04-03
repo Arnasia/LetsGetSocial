cb1a1797513cf9b17bbcd9679efb4fc102e5fdc2
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-04-03 14:49:51 -0400

    add function to get all thoughts and by ID

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,3 +1,35 @@
 const { User, Thought } = require('../models');
 
 
+const userController = {
+    // get all thoughts
+    getAllUser(req, res) {
+        User.find({})
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+    // get one thought by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.userId })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+}
