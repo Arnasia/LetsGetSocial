49ca11a363474bec00b55b078560ab770790a1ab
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-04-03 15:35:46 -0400

    subdoc for thoughts

--- models/User.js
+++ models/User.js
@@ -1,1 +1,41 @@
 const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+            //match a valid email address
+        },
+        //subdoc for thoughts
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                //referring to thought document model
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                //referring to user document model
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
