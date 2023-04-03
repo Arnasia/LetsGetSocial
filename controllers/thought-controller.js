276ea5805ca1c59b59452a0e60c4c41c99c4614e
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-04-03 14:21:47 -0400

    functionality to add a reaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -41,4 +41,21 @@ const thoughtController = {
             .catch(err => res.json(err));
     },
 
+      // add a reaction to thought
+      addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body } },
+            { new: true, runValidators: true }
+        )
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
+    },
+
 }
