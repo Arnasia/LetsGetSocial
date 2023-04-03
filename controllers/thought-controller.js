b18f8f9ee274a123c863528902b4ac6cdc2655c2
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-04-03 13:47:35 -0400

    attempting to commit

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,3 +1,27 @@
 // Importing Thought and User models
 const { Thought, User } = require('../models');
 
+const thoughtController = {
+    // get all thoughts
+    getAllThought(req, res) {
+        Thought.find({})
+            .select('-__v')
+            .sort({ createdAt: -1 })
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+     // get one thought by id
+     getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.thoughtId })
+            .select('-__v')
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+}
